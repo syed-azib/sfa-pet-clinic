98422df6e18fa59dba9c811bddcebfaf708e23ac
Syed Azib <syed.azib@hotmail.com>
2020-09-21 19:53:32 -0400

    project setup

---
+++ src/main/java/azib/springframework/sfapetclinic/SfaPetClinicApplication.java
@@ -0,0 +1,13 @@
+package azib.springframework.sfapetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfaPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfaPetClinicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/azib/springframework/sfapetclinic/SfaPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package azib.springframework.sfapetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfaPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
