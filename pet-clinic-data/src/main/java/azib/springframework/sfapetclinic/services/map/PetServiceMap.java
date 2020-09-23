5ed08218fc035cb8fb37e6b24595b8b309dd135d
Syed Azib <syed.azib@hotmail.com>
2020-09-23 16:20:01 -0400

    Impl map based service, closes #6

---
+++ pet-clinic-data/src/main/java/azib/springframework/sfapetclinic/services/map/PetServiceMap.java
@@ -0,0 +1,34 @@
+package azib.springframework.sfapetclinic.services.map;
+
+import azib.springframework.sfapetclinic.model.Pet;
+import azib.springframework.sfapetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+}
