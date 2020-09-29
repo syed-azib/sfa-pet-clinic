8837ab9b57d3c97be340ff172e6343a0d7015d25
Syed Azib <syed.azib@hotmail.com>
2020-09-29 17:19:25 -0400

    added properties. closes #28

--- pet-clinic-data/src/main/java/azib/springframework/sfapetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/azib/springframework/sfapetclinic/model/Owner.java
@@ -4,7 +4,34 @@ import java.util.Set;
 
 public class Owner extends Person{
 
+    private String adddress;
+    private String city;
+    private String telephone;
     private Set<Pet> pets;
+
+    public String getAdddress() {
+        return adddress;
+    }
+
+    public void setAdddress(String adddress) {
+        this.adddress = adddress;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
 
     public Set<Pet> getPets() {
         return pets;
