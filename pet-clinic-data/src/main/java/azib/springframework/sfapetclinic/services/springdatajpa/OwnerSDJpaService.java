2a04fdcde404ccc6eff25607c86ad6f5e7802e5f
Syed Azib <syed.azib@hotmail.com>
2020-10-18 16:36:13 -0400

    displayed owner record. closes #53

--- pet-clinic-data/src/main/java/azib/springframework/sfapetclinic/services/springdatajpa/OwnerSDJpaService.java
+++ pet-clinic-data/src/main/java/azib/springframework/sfapetclinic/services/springdatajpa/OwnerSDJpaService.java
@@ -23,6 +23,7 @@ public class OwnerSDJpaService implements OwnerService {
 
     public OwnerSDJpaService(OwnerRepository ownerRepository, PetRepository petRepository, PetTypeRepository petTypeRepository) {
         this.ownerRepository = ownerRepository;
+
         this.petRepository = petRepository;
         this.petTypeRepository = petTypeRepository;
     }
