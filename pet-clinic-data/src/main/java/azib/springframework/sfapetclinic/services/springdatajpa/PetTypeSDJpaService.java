f47699a00f6c9e4c33667f180401e75fec1735d1
Syed Azib <syed.azib@hotmail.com>
2020-10-18 16:28:37 -0400

    fix display owner. Closes #59

--- pet-clinic-data/src/main/java/azib/springframework/sfapetclinic/services/springdatajpa/PetTypeSDJpaService.java
+++ pet-clinic-data/src/main/java/azib/springframework/sfapetclinic/services/springdatajpa/PetTypeSDJpaService.java
@@ -16,36 +16,36 @@ import java.util.Set;
 @Service
 @Profile("springdatajpa")
 public class PetTypeSDJpaService implements PetTypeService {
-    private final PetTypeRepository petRepository;
+    private final PetTypeRepository petTypeRepository;
 
-    public PetTypeSDJpaService(PetTypeRepository petRepository) {
-        this.petRepository = petRepository;
+    public PetTypeSDJpaService(PetTypeRepository petTypeRepository) {
+        this.petTypeRepository = petTypeRepository;
     }
 
     @Override
     public Set<PetType> findAll() {
-        Set<PetType> pets = new HashSet<>();
-        petRepository.findAll().forEach(pets::add);
-        return pets;
+        Set<PetType> petTypes = new HashSet<>();
+        petTypeRepository.findAll().forEach(petTypes::add);
+        return petTypes;
     }
 
     @Override
     public PetType findById(Long aLong) {
-        return petRepository.findById(aLong).orElse(null);
+        return petTypeRepository.findById(aLong).orElse(null);
     }
 
     @Override
     public PetType save(PetType object) {
-        return null;
+        return petTypeRepository.save(object);
     }
 
     @Override
     public void delete(PetType object) {
-
+        petTypeRepository.delete(object);
     }
 
     @Override
     public void deleteById(Long aLong) {
-        petRepository.deleteById(aLong);
+        petTypeRepository.deleteById(aLong);
     }
 }
